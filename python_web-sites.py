4cad5b1f4da4dfc012e2fbba17b2a43c99b80d1f
YvonneOZertuche <aymzertuche1@outlook.com>
2022-07-13 09:12:25 -0500

    Checking to see if the repo is working properly - 2nd commit

--- python_web-sites.py
+++ python_web-sites.py
@@ -4,3 +4,6 @@ https://diveintopython3.problemsolving.io/ #--->
 #~A negative index accesses items from the end of the list counting backwards a_list[-1]
 #~Can use the 'in' operator to return whether a value appears in a list - it will return True or False
 #~ Using the index() method tells you WHERE in the list a value appears. If value is not found the index()method will notify you by returning FALSE
+
+
+Changes
