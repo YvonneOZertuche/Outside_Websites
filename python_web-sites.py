b76308f95e4493d0e5fde96c5daea753f329fdec
YvonneOZertuche <aymzertuche1@outlook.com>
2022-07-13 09:06:57 -0500

    First Commit

---
+++ python_web-sites.py
@@ -0,0 +1,6 @@
+https://diveintopython3.problemsolving.io/ #--->
+#!LISTS - sequence
+#~ List is an ordered set of items
+#~A negative index accesses items from the end of the list counting backwards a_list[-1]
+#~Can use the 'in' operator to return whether a value appears in a list - it will return True or False
+#~ Using the index() method tells you WHERE in the list a value appears. If value is not found the index()method will notify you by returning FALSE
